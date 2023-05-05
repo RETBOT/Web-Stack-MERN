5cd523c78f9670841c7ad42e66ff7aac7a5d44bb
RETBOT <robertoesquiveltr16@gmail.com>
2023-05-05 12:35:53 -0600

    FrontEnd Contacto

--- retbot/src/pages/web/Contact/Contact.js
+++ retbot/src/pages/web/Contact/Contact.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react';
+import { map } from 'lodash';
+import { socialData } from '../../../utils'
+import "./Contact.scss"
 
 export function Contact() {
   return (
-    <div>
-      <h1>Estamos en contactos</h1>
+    <div class="redes-sociales">
+      <div className='red-social'>
+        <span class="red-social-nombre">Contacto: </span>
+      </div>
+      {map(socialData, (social) => (
+        <div className='red-social'>
+          <span class="red-social-nombre">{social.type}</span>
+          <Button
+            className='red-social-icono'
+            key={social.type}
+            as="a"
+            target="_blank"
+            href={social.link}
+            color={social.type}
+            icon={social.type} />
+        </div>
+      ))}
+
     </div>
   )
 }
---
+++ server/constants.js
@@ -0,0 +1,17 @@
+// Configurar en atlas db
+const DB_USER = "USER";
+const DB_PASSWORD = "PASSWORD";
+const DB_HOST = "HOST";
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+
+const JWT_SECRET_KEY = "asjhdajshdbajsdbajsbdhjashdb";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY,
+}
